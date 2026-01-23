398db13bda97af16b3cfd66260b065ae0fc2c813
Sagar Vavadiya <sva@taglineinfotech.com>
2026-01-23 12:55:41 +0530

    redux setup

--- inventoryManagement/frontend/src/app/page.js
+++ inventoryManagement/frontend/src/app/page.js
@@ -1,5 +1,4 @@
 import LoginComponent from "@/components/pageComponents/Login/LoginComponent";
-import Image from "next/image";
 
 export default function LoginPage() {
   return <LoginComponent />;
